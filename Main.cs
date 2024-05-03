f8592f3fbc788a2d9b96adb2c7cb3a46b21d06f7
Alexandre Ovrutskiy <168657039+AlexOvrGit@users.noreply.github.com>
2024-05-03 01:17:07 -0400

    Add Main

--- Main.cs
+++ Main.cs
@@ -18,9 +18,10 @@ namespace FastFactorialTest
                 iTo = int.Parse(args[nArg + 2]);
                 Console.WriteLine("Diff={0}, From={1}, To={2}",
                     iDiff, iFrom, iTo);
-                long lStartTime = sw.ElapsedMilliseconds;
+                // long lStartTime = sw.ElapsedMilliseconds;
+                sw.Restart();
                 BigInteger bigProduct = FastFactorial(iDiff, iFrom, iTo);
-                lCountTime = sw.ElapsedMilliseconds - lStartTime;
+                lCountTime = sw.ElapsedMilliseconds;
                 Console.WriteLine("Product = {0:N0}", bigProduct);
                 Console.WriteLine("Count Time = {0}ms, Output Time = {1}ms",
                   lCountTime, sw.ElapsedMilliseconds - lCountTime);
